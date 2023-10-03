d42c8e99ee57e78c10773ecaec9de9fbf7e2b0ef
Naman Bhatt <naman.bhatt@gdn-commerce.com>
2023-10-03 22:08:26 +0530

    CleverTap SDK intigration

--- app/src/main/java/com/example/tap/MainActivity.kt
+++ app/src/main/java/com/example/tap/MainActivity.kt
@@ -2,15 +2,28 @@ package com.example.tap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clevertap.android.sdk.CleverTapAPI
 import com.example.tap.databinding.ActivityMainBinding
+import com.example.tap.model.EventData
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var binding : ActivityMainBinding
+  var cleverTapDefaultInstance: CleverTapAPI? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    cleverTapDefaultInstance = CleverTapAPI.getDefaultInstance(applicationContext)
+
+    binding.btnTap.setOnClickListener {
+      EventData(
+        productId = "1",
+        productImage = "https://d35fo82fjcw0y8.cloudfront.net/2018/07/26020307/customer-success-clevertap.jpg",
+        productName = "CleverTap"
+      )
+    }
   }
 }
---
+++ app/src/main/java/com/example/tap/model/EventData.kt
@@ -0,0 +1,5 @@
+package com.example.tap.model
+
+data class EventData(
+  private val productId: String, private val productImage: String, private val productName: String
+)
